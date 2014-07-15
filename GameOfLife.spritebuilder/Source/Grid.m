3103f3346cad6c220fc87d443d5cf8b039757f68
bastian153 <ruben@rubens-mbp-2.gateway.pace.com>
2014-07-14 18:36:33 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -19,6 +19,73 @@ static const int GRID_COLUMNS = 10;
     float _cellHeight;
 }
 
+
+
+- (void)onEnter
+{
+    [super onEnter];
+    
+    [self setupGrid];
+    
+    // accept touches on the grid
+    self.userInteractionEnabled = YES;
+}
+
+- (void)setupGrid
+{
+    // divide the grid's size by the number of columns/rows to figure out the right width and height of each cell
+    _cellWidth = self.contentSize.width / GRID_COLUMNS;
+    _cellHeight = self.contentSize.height / GRID_ROWS;
+    
+    float x = 0;
+    float y = 0;
+    
+    // initialize the array as a blank NSMutableArray
+    _gridArray = [NSMutableArray array];
+    
+    // initialize Creatures
+    for (int i = 0; i < GRID_ROWS; i++) {
+        // this is how you create two dimensional arrays in Objective-C. You put arrays into arrays.
+        _gridArray[i] = [NSMutableArray array];
+        x = 0;
+        
+        for (int j = 0; j < GRID_COLUMNS; j++) {
+            Creature *creature = [[Creature alloc] initCreature];
+            creature.anchorPoint = ccp(0, 0);
+            creature.position = ccp(x, y);
+            [self addChild:creature];
+            
+            // this is shorthand to access an array inside an array
+            _gridArray[i][j] = creature;
+            
+            x+=_cellWidth;
+        }
+        
+        y += _cellHeight;
+    }
+}
+
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    //get the x,y coordinates of the touch
+    CGPoint touchLocation = [touch locationInNode:self];
+    
+    //get the Creature at that location
+    Creature *creature = [self creatureForTouchPosition:touchLocation];
+    
+    //invert it's state - kill it if it's alive, bring it to life if it's dead.
+    creature.isAlive = !creature.isAlive;
+}
+
+- (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
+{
+    //get the row and column that was touched, return the Creature inside the corresponding cell
+    int row = touchPosition.y/ _cellHeight;
+    int column = touchPosition.x/ _cellWidth;
+    
+    return _gridArray[row][column];
+}
+
 - (void)evolveStep
 {
     //update each Creature's neighbor count
@@ -107,70 +174,4 @@ static const int GRID_COLUMNS = 10;
     
     _totalAlive = numAlive;
 }
-
-- (void)onEnter
-{
-    [super onEnter];
-    
-    [self setupGrid];
-    
-    // accept touches on the grid
-    self.userInteractionEnabled = YES;
-}
-
-- (void)setupGrid
-{
-    // divide the grid's size by the number of columns/rows to figure out the right width and height of each cell
-    _cellWidth = self.contentSize.width / GRID_COLUMNS;
-    _cellHeight = self.contentSize.height / GRID_ROWS;
-    
-    float x = 0;
-    float y = 0;
-    
-    // initialize the array as a blank NSMutableArray
-    _gridArray = [NSMutableArray array];
-    
-    // initialize Creatures
-    for (int i = 0; i < GRID_ROWS; i++) {
-        // this is how you create two dimensional arrays in Objective-C. You put arrays into arrays.
-        _gridArray[i] = [NSMutableArray array];
-        x = 0;
-        
-        for (int j = 0; j < GRID_COLUMNS; j++) {
-            Creature *creature = [[Creature alloc] initCreature];
-            creature.anchorPoint = ccp(0, 0);
-            creature.position = ccp(x, y);
-            [self addChild:creature];
-            
-            // this is shorthand to access an array inside an array
-            _gridArray[i][j] = creature;
-            
-            x+=_cellWidth;
-        }
-        
-        y += _cellHeight;
-    }
-}
-
-- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
-{
-    //get the x,y coordinates of the touch
-    CGPoint touchLocation = [touch locationInNode:self];
-    
-    //get the Creature at that location
-    Creature *creature = [self creatureForTouchPosition:touchLocation];
-    
-    //invert it's state - kill it if it's alive, bring it to life if it's dead.
-    creature.isAlive = !creature.isAlive;
-}
-
-- (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
-{
-    //get the row and column that was touched, return the Creature inside the corresponding cell
-    int row = touchPosition.y/ _cellHeight;
-    int column = touchPosition.x/ _cellWidth;
-    
-    return _gridArray[row][column];
-}
-
 @end
